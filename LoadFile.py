ac05e2b71d750697e4c8a6c813ac0959ab94ecbe
reynull20 <muhammadreyhan2000@gmail.com>
2021-04-05 02:32:34 +0700

    add load File

---
+++ LoadFile.py
@@ -0,0 +1,32 @@
+from Graph import Graph
+import math
+
+def loadToGraf(filename, grafDest):
+    filebuff = open("./"+filename)
+    listofnode = []
+    
+    i = 0
+    for item in filebuff:
+        if (i == 0):
+            count = int(item)
+        elif(i < count+1):
+            temp = item.strip().split(" ")
+            grafDest.addNode(temp[0], float(temp[1]), float(temp[2]))
+            listofnode.append(temp[0])
+        else:
+            temp1 = item.strip().split(" ")
+            for j in range(len(temp1)):
+                if(temp1[j] != "0" and temp1[j] != "0.0"):
+                    tempX1, tempY1 = grafDest.getPos(listofnode[i-count-1])
+                    tempX2, tempY2 = grafDest.getPos(listofnode[j])
+                    tempWeight = math.sqrt(math.pow((tempX1 - tempX2),2) + math.pow((tempY1 - tempY2),2))
+                    grafDest.addEdge(listofnode[i-count-1], listofnode[j], tempWeight)
+        i += 1
+'''
+g1 = Graph()
+loadToGraf("test.txt", g1)
+for item in g1.graf.keys():
+    for item2 in g1.graf[item].keys():
+        print(str(g1.graf[item][item2]), end=" ")
+    print()
+'''
---
+++ test.py
@@ -0,0 +1,5 @@
+import Graph, Astar, LoadFile
+
+g1 = Graph()
+ans = Astar()
+LoadFile.loadToGraf("test.txt",g1)
